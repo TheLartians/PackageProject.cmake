a1dc019f532338a3577c5898cb3d6c0c453ef7a7
Lars Melchior <TheLartians@users.noreply.github.com>
2020-06-03 16:01:08 +0200

    Create version header (#5)
    * create version header
    
    * use correct return code
    
    * create real dependency target
    
    * use correct install destination
    
    * use sudo
    
    * update docs

---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+#include <dependency/dependency.h>
+#include <dependency/version.h>
+#include <string>
+
+int main() {
+  dependencyFunction();
+  return DEPENDENCY_VERSION == std::string("1.2.3") ? 0 : 1;
+}
