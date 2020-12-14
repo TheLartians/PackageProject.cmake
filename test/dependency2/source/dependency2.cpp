2fdf51c6d3808d84e61fa3549fe349cfce32bbf4
Michael Unknown <michael_knoerzer@gmx.net>
2020-12-14 20:16:00 +0100

    fix ns typo

--- test/dependency2/source/dependency2.cpp
+++ test/dependency2/source/dependency2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <dependency2/version.h>
 
-void ns::dependencyFunction2() {
-  std::cout << "Using dependency version " << DEPENDENCY_VERSION << std::endl;
+namespace ns {
+	void dependencyFunction2() {
+		std::cout << "Using dependency2 version " << DEPENDENCY2_VERSION << std::endl;
+	}
 }
