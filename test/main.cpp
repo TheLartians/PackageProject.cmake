918b237427f82f96d3e80550c374f4133a2f6ea0
Lars Melchior <TheLartians@users.noreply.github.com>
2021-02-08 12:34:34 +0100

    allow passing versioned args

--- test/main.cpp
+++ test/main.cpp
@@ -2,10 +2,17 @@
 #include <dependency/version.h>
 #include <namespaced_dependency/namespaced_dependency.h>
 #include <namespaced_dependency/version.h>
+#include <transitive_dependency/transitive_dependency.h>
+#include <transitive_dependency/version.h>
 #include <string>
 
 int main() {
   dependencyFunction();
   ns::namespacedDependencyFunction();
-  return DEPENDENCY_VERSION == std::string("1.2.3") && NAMESPACED_DEPENDENCY_VERSION == std::string("4.5.6") ? 0 : 1;
+  transitiveDependencyFunction();
+  auto result = true;
+  result &= DEPENDENCY_VERSION == std::string("1.2.3");
+  result &= NAMESPACED_DEPENDENCY_VERSION == std::string("4.5.6");
+  result &= TRANSITIVE_DEPENDENCY_VERSION == std::string("7.8.9");
+  return result ? 0 : 1;
 }
