16b9cd599b562a5751b4be6063d3c4cbdf4b166d
Lars Melchior <TheLartians@users.noreply.github.com>
2021-02-15 12:01:39 +0100

    Add code formatting (#17)
    * add style config and enforce formatting
    
    * remove leading underscore from var name

--- test/namespaced_dependency/source/namespaced_dependency.cpp
+++ test/namespaced_dependency/source/namespaced_dependency.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
 #include <namespaced_dependency/version.h>
 
+#include <iostream>
+
 namespace ns {
-	void namespacedDependencyFunction() {
-		std::cout << "Using namespaced_dependency version " << NAMESPACED_DEPENDENCY_VERSION << std::endl;
-	}
-}
+  void namespacedDependencyFunction() {
+    std::cout << "Using namespaced_dependency version " << NAMESPACED_DEPENDENCY_VERSION
+              << std::endl;
+  }
+}  // namespace ns
