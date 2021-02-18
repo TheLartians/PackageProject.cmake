43edc79007ae8969ded36567edf8bf6956f63c76
ClausKlein <claus.klein@arcormail.de>
2021-02-18 21:33:50 +0100

    add handling to install dll too
    modernize cmake files too

--- test/transitive_dependency/source/transitive_dependency.cpp
+++ test/transitive_dependency/source/transitive_dependency.cpp
@@ -1,6 +1,7 @@
 #include <fmt/format.h>
+#include <transitive_dependency/export.h>
 #include <transitive_dependency/version.h>
 
-void transitiveDependencyFunction() {
+void TRANSITIVE_DEPENDENCY_EXPORT transitiveDependencyFunction() {
   fmt::print("Using transitive_dependency version {}\n", TRANSITIVE_DEPENDENCY_VERSION);
 }
