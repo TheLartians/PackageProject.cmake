84b59e591a659f4ab61aedf6f9903a5cfda105c3
Arniiiii <lg3dx6fd@gmail.com>
2024-08-21 02:56:46 +0000

    add test
    added test for header only, though it works only in github workflow
    
    added some folders to gitignore
    
    made github workflow to install not on a system but in a folder `install_dir`
    
    formatted some CMakeLists.txt with `cmake-format`

---
+++ test/header_only/include/header_only/adder.h
@@ -0,0 +1,5 @@
+#pragma once
+
+inline constexpr long add(int a, int b) {
+  return a + b;
+}
--- test/main.cpp
+++ test/main.cpp
@@ -4,6 +4,8 @@
 #include <namespaced_dependency/version.h>
 #include <transitive_dependency/transitive_dependency.h>
 #include <transitive_dependency/version.h>
+#include <header_only/adder.h>
+#include <header_only/version.h>
 
 #include <string>
 
@@ -27,5 +29,11 @@ int main() {
   result &= TRANSITIVE_DEPENDENCY_VERSION_MINOR == 8;
   result &= TRANSITIVE_DEPENDENCY_VERSION_PATCH == 9;
   result &= TRANSITIVE_DEPENDENCY_VERSION_TWEAK == 21948124;
+  result &= (5 == add(2,3));
+  result &= HEADER_ONLY_VERSION == std::string("1.2");
+  result &= HEADER_ONLY_VERSION_MAJOR == 1;
+  result &= HEADER_ONLY_VERSION_MINOR == 2;
+  result &= HEADER_ONLY_VERSION_PATCH == 0;
+  result &= HEADER_ONLY_VERSION_TWEAK == 0;
   return result ? 0 : 1;
 }
