5f51898d02176e2dcf4b07d773062bdd27e00180
Arni <lg3dx6fd@gmail.com>
2024-09-02 15:24:58 +0000

    make ARCH_INDEPENDENT libraries to have CMake configs installed to share (#42)
    * make ARCH_INDEPENDENT libraries to have CMake configs installed to share
    
    * add test
    
    added test for header only, though it works only in github workflow
    
    added some folders to gitignore
    
    made github workflow to install not on a system but in a folder `install_dir`
    
    formatted some CMakeLists.txt with `cmake-format`
    
    * micro fix of test for header_only
    
    * Update .gitignore
    
    Co-authored-by: Lars Melchior <TheLartians@users.noreply.github.com>
    
    * maybe fix CI
    
    * fix formatting
    
    * fix formatting.
    
    * maybe fix tests
    
    * place comment to the end of command, not as separate command.
    
    * check for cmake config in /usr/local/share, not /usr/share
    
    ---------
    
    Co-authored-by: Lars Melchior <TheLartians@users.noreply.github.com>

---
+++ test/header_only/include/header_only/adder.h
@@ -0,0 +1,3 @@
+#pragma once
+
+inline constexpr long add(int a, int b) { return a + b; }
--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,7 @@
 #include <dependency/dependency.h>
 #include <dependency/version.h>
+#include <header_only/adder.h>
+#include <header_only/version.h>
 #include <namespaced_dependency/namespaced_dependency.h>
 #include <namespaced_dependency/version.h>
 #include <transitive_dependency/transitive_dependency.h>
@@ -27,5 +29,11 @@ int main() {
   result &= TRANSITIVE_DEPENDENCY_VERSION_MINOR == 8;
   result &= TRANSITIVE_DEPENDENCY_VERSION_PATCH == 9;
   result &= TRANSITIVE_DEPENDENCY_VERSION_TWEAK == 21948124;
+  result &= (5 == add(2, 3));
+  result &= HEADER_ONLY_VERSION == std::string("1.0");
+  result &= HEADER_ONLY_VERSION_MAJOR == 1;
+  result &= HEADER_ONLY_VERSION_MINOR == 0;
+  result &= HEADER_ONLY_VERSION_PATCH == 0;
+  result &= HEADER_ONLY_VERSION_TWEAK == 0;
   return result ? 0 : 1;
 }
